9b714dd8a21b4084d5407cbf90d3a5e083221a8f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 14:11:23 +0100

    Created GratitudeResponse

---
+++ src/TaaS.Api.WebApi/Model/V1/GratitudeResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace TaaS.Api.WebApi.Model.V1
+{
+    public class GratitudeResponse
+    {
+        public GratitudeResponse(int id, string text)
+        {
+            Id = id;
+            Text = text;
+        }
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
