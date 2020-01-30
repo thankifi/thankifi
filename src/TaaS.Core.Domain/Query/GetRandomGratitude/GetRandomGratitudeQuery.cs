673f4e7567186b09826191665e4d1c5072919300
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 12:16:13 +0100

    Started with GetRandomGratitude

---
+++ src/TaaS.Core.Domain/Query/GetRandomGratitude/GetRandomGratitudeQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace TaaS.Core.Domain.Query.GetRandomGratitude
+{
+    public class GetRandomGratitudeQuery : IRequest<string>
+    {
+        public GetRandomGratitudeQuery(string name = "Alice", string signature = "Bob")
+        {
+            Name = name;
+            Signature = signature;
+        }
+
+        public string Name { get; }
+        public string Signature { get; }
+    }
+}
---
+++ src/TaaS.Core.Domain/Query/GetRandomGratitude/GetRandomGratitudeQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace TaaS.Core.Domain.Query.GetRandomGratitude
+{
+    public class GetRandomGratitudeQueryHandler : IRequestHandler<GetRandomGratitudeQuery, string>
+    {
+        public Task<string> Handle(GetRandomGratitudeQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
