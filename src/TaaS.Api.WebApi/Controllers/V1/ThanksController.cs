97140d027695d3a827858a84c7a8f9dfa1f33202
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 15:03:34 +0100

    Updated thanks controller documentation

--- src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
+++ src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
@@ -6,7 +6,10 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TaaS.Api.WebApi.Model.V1;
+using TaaS.Core.Domain.Query.GetGratitudeById;
 using TaaS.Core.Domain.Query.GetGratitudeRandom;
+using TaaS.Core.Domain.Query.GetGratitudeRandomByType;
+using TaaS.Core.Entity;
 
 namespace TaaS.Api.WebApi.Controllers.V1
 {
@@ -26,13 +29,13 @@ namespace TaaS.Api.WebApi.Controllers.V1
         }
 
         /// <summary>
-        /// Get a gratitude sentence by Id, optionally specify a name and a signature.
+        /// Get a gratitude sentence by Id, optionally specify a name and a signature. Thanks!
         /// </summary>
         /// <param name="id"></param>
         /// <param name="name"></param>
         /// <param name="signature"></param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
         [HttpGet, Route("{id}")]
         [ProducesResponseType(typeof(GratitudeResponse), 200)]
         public async Task<IActionResult> GetById(
@@ -41,11 +44,19 @@ namespace TaaS.Api.WebApi.Controllers.V1
             [FromQuery, DefaultValue("Bob")] string signature,
             CancellationToken cancellationToken)
         {
-            var gratitude = await Mediator.Send(null, cancellationToken);
-            return Ok();
+            var (_, text) = await Mediator.Send(new GetGratitudeByIdQuery(id, name, signature), cancellationToken);
+            return Ok(new GratitudeResponse(id, text));
         }
 
+        /// <summary>
+        /// Get a random gratitude sentence, optionally specify a name and a signature. Thanks!
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="signature"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
         [HttpGet, Route("random")]
+        [ProducesResponseType(typeof(GratitudeResponse), 200)]
         public async Task<IActionResult> GetRandom(
             [FromQuery, DefaultValue("Alice")] string name,
             [FromQuery, DefaultValue("Bob")] string signature,
@@ -56,31 +67,71 @@ namespace TaaS.Api.WebApi.Controllers.V1
             return Ok(new GratitudeResponse(id, text));
         }
         
+        /// <summary>
+        /// Get a random gratitude basic sentence. No names, no signatures. Thanks!
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
         [HttpGet, Route("random/basic")]
-        public async Task<IActionResult> GetRandomNamed(
+        [ProducesResponseType(typeof(GratitudeResponse), 200)]
+        public async Task<IActionResult> GetRandomBasic(
             CancellationToken cancellationToken)
         {
-            var (id, text) = await Mediator.Send(new GetGratitudeRandomQuery(), cancellationToken);
+            var (id, text) = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.Basic), cancellationToken);
 
             return Ok(new GratitudeResponse(id, text));
         }
 
+        /// <summary>
+        /// Get a random gratitude named sentence. Required name, no signatures. Thanks!
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
         [HttpGet, Route("random/named")]
+        [ProducesResponseType(typeof(GratitudeResponse), 200)]
         public async Task<IActionResult> GetRandomNamed(
-            [FromQuery, DefaultValue("Alice")] string name,
+            [FromQuery, Required] string name,
             CancellationToken cancellationToken)
         {
-            var (id, text) = await Mediator.Send(new GetGratitudeRandomQuery(name, ""), cancellationToken);
+            var (id, text) = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.Named, name), cancellationToken);
 
             return Ok(new GratitudeResponse(id, text));
         }
 
+        /// <summary>
+        /// Get a random gratitude signed sentence. No name, required signature. Thanks!
+        /// </summary>
+        /// <param name="signature"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
         [HttpGet, Route("random/signed")]
+        [ProducesResponseType(typeof(GratitudeResponse), 200)]
         public async Task<IActionResult> GetRandomSigned(
-            [FromQuery, DefaultValue("Bob")] string signature,
+            [FromQuery, Required] string signature,
             CancellationToken cancellationToken)
         {
-            var (id, text) = await Mediator.Send(new GetGratitudeRandomQuery("", signature), cancellationToken);
+            var (id, text) = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.Signed, signature: signature), cancellationToken);
+
+            return Ok(new GratitudeResponse(id, text));
+        }
+            
+        /// <summary>
+        /// Get a random gratitude named and signed. Required name, required signature. Thanks!
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="signature"></param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
+        [HttpGet, Route("random/namedAndSigned")]
+        [ProducesResponseType(typeof(GratitudeResponse), 200)]
+        public async Task<IActionResult> GetRandomNamedAndSigned(
+            [FromQuery, Required] string name,
+            [FromQuery, Required] string signature,
+            CancellationToken cancellationToken)
+        {
+            var (id, text) = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.NamedAndSigned, name, signature), cancellationToken);
 
             return Ok(new GratitudeResponse(id, text));
         }
