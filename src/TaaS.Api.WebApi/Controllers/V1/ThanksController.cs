211a80c5b3add5bc24554008cccdef24b632aeab
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 12:56:24 +0100

    Added ThanksController

---
+++ src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaaS.Api.WebApi.Controllers.V1
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{v:apiVersion}/[controller]")]
+    public class ThanksController : ControllerBase
+    {
+        
+    }
+}
