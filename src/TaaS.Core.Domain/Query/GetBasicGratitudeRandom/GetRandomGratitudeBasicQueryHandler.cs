99d972fd667a9ab88f6b1358ea0431a675cd8333
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 14:19:31 +0100

    Implemented GetRandomGratitudeBasicQuery handler

---
+++ src/TaaS.Core.Domain/Query/GetBasicGratitudeRandom/GetRandomGratitudeBasicQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TaaS.Common.Helper;
+using TaaS.Core.Entity;
+using TaaS.Persistence.Context;
+
+namespace TaaS.Core.Domain.Query.GetBasicGratitudeRandom
+{
+    public class GetRandomGratitudeBasicQueryHandler : IRequestHandler<GetRandomGratitudeBasicQuery, (int, string)>
+    {
+        protected readonly ILogger<GetRandomGratitudeBasicQueryHandler> Logger;
+        protected readonly TaaSDbContext Context;
+
+        public GetRandomGratitudeBasicQueryHandler(ILogger<GetRandomGratitudeBasicQueryHandler> logger, TaaSDbContext context)
+        {
+            Logger = logger;
+            Context = context;
+        }
+
+        public async Task<(int, string)> Handle(GetRandomGratitudeBasicQuery request, CancellationToken cancellationToken)
+        {
+            Logger.LogDebug("Requested random basic gratitude.");
+            
+            var offset = RandomProvider.GetThreadRandom().Next(0, await Context.Gratitudes.Where(g => g.Type == GratitudeType.Basic).CountAsync(cancellationToken));
+
+            var gratitude = await Context.Gratitudes.Where(g => g.Type == GratitudeType.Basic).Skip(offset).FirstAsync(cancellationToken);
+
+            var response = gratitude.Text;
+
+            return (gratitude.Id, response);
+        }
+    }
+}
