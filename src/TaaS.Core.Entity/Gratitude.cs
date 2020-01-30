bd551a83e978d7a8fd537c72da2f4c829c04d38b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 12:05:50 +0100

    Added Gratitude entity

---
+++ src/TaaS.Core.Entity/Gratitude.cs
@@ -0,0 +1,18 @@
+﻿namespace TaaS.Core.Entity
+{
+    public class Gratitude
+    {
+        public int Id { get; set; }
+        public string Language { get; set; }
+        public GratitudeType Type { get; set; }
+        public string Text { get; set; }
+    }
+
+    public enum GratitudeType
+    {
+        Standard,
+        Named,
+        Signed,
+        NamedAndSigned
+    }
+}
