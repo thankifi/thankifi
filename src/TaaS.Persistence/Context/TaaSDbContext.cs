c4b00c1f3455dab5746f1acb487458e387d8cef2
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 12:18:41 +0100

    Added DbContext

---
+++ src/TaaS.Persistence/Context/TaaSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaaS.Core.Entity;
+
+namespace TaaS.Persistence
+{
+    public class TaaSDbContext : DbContext
+    {
+        public DbSet<Gratitude> Gratitudes { get; set; }
+
+        protected TaaSDbContext()
+        {
+        }
+
+        public TaaSDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
