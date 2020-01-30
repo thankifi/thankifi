56b79d82dad1bbfb2d6b81fa2217a55198f7b961
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 14:11:15 +0100

    Implemented some methods in ThanksController

--- src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
+++ src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
@@ -1,12 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TaaS.Api.WebApi.Model.V1;
+using TaaS.Core.Domain.Query.GetGratitudeRandom;
 
 namespace TaaS.Api.WebApi.Controllers.V1
 {
     [ApiController]
     [ApiVersion("1")]
+    [Produces("application/json")]
     [Route("api/v{v:apiVersion}/[controller]")]
     public class ThanksController : ControllerBase
     {
+        protected readonly ILogger<ThanksController> Logger;
+        protected readonly IMediator Mediator;
+
+        public ThanksController(ILogger<ThanksController> logger, IMediator mediator)
+        {
+            Logger = logger;
+            Mediator = mediator;
+        }
+
+        /// <summary>
+        /// Get a gratitude sentence by Id, optionally specify a name and a signature.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="signature"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet, Route("{id}")]
+        [ProducesResponseType(typeof(GratitudeResponse), 200)]
+        public async Task<IActionResult> GetById(
+            [FromRoute, Required] int id,
+            [FromQuery, DefaultValue("Alice")] string name,
+            [FromQuery, DefaultValue("Bob")] string signature,
+            CancellationToken cancellationToken)
+        {
+            var gratitude = await Mediator.Send(null, cancellationToken);
+            return Ok();
+        }
+
+        [HttpGet, Route("random")]
+        public async Task<IActionResult> GetRandom(
+            [FromQuery, DefaultValue("Alice")] string name,
+            [FromQuery, DefaultValue("Bob")] string signature,
+            CancellationToken cancellationToken)
+        {
+            var (id, text) = await Mediator.Send(new GetRandomGratitudeQuery(name, signature), cancellationToken);
+
+            return Ok(new GratitudeResponse(id, text));
+        }
         
+        [HttpGet, Route("random/basic")]
+        public async Task<IActionResult> GetRandomNamed(
+            CancellationToken cancellationToken)
+        {
+            var (id, text) = await Mediator.Send(new GetRandomGratitudeQuery(), cancellationToken);
+
+            return Ok(new GratitudeResponse(id, text));
+        }
+
+        [HttpGet, Route("random/named")]
+        public async Task<IActionResult> GetRandomNamed(
+            [FromQuery, DefaultValue("Alice")] string name,
+            CancellationToken cancellationToken)
+        {
+            var (id, text) = await Mediator.Send(new GetRandomGratitudeQuery(name, ""), cancellationToken);
+
+            return Ok(new GratitudeResponse(id, text));
+        }
+
+        [HttpGet, Route("random/signed")]
+        public async Task<IActionResult> GetRandomSigned(
+            [FromQuery, DefaultValue("Bob")] string signature,
+            CancellationToken cancellationToken)
+        {
+            var (id, text) = await Mediator.Send(new GetRandomGratitudeQuery("", signature), cancellationToken);
+
+            return Ok(new GratitudeResponse(id, text));
+        }
     }
 }
