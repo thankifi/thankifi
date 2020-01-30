42816af53489a5381418e784b4ae8f227b98abc3
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 15:35:00 +0100

    Added language to queries

--- src/TaaS.Core.Domain/Query/GetGratitudeRandom/GetGratitudeRandomQuery.cs
+++ src/TaaS.Core.Domain/Query/GetGratitudeRandom/GetGratitudeRandomQuery.cs
@@ -4,12 +4,14 @@ namespace TaaS.Core.Domain.Query.GetGratitudeRandom
 {
     public class GetGratitudeRandomQuery : IRequest<(int, string)>
     {
-        public GetGratitudeRandomQuery(string name = "Alice", string signature = "Bob")
+        public GetGratitudeRandomQuery(string language = "en", string name = "Alice", string signature = "Bob")
         {
+            Language = language.ToLower();
             Name = name;
             Signature = signature;
         }
 
+        public string Language { get; }
         public string Name { get; }
         public string Signature { get; }
     }
--- src/TaaS.Core.Domain/Query/GetGratitudeRandomByType/GetGratitudeRandomByTypeQuery.cs
+++ src/TaaS.Core.Domain/Query/GetGratitudeRandomByType/GetGratitudeRandomByTypeQuery.cs
@@ -5,13 +5,15 @@ namespace TaaS.Core.Domain.Query.GetGratitudeRandomByType
 {
     public class GetGratitudeRandomByTypeQuery : IRequest<(int, string)>
     {
-        public GetGratitudeRandomByTypeQuery(GratitudeType type, string name = "Alice", string signature = "Bob")
+        public GetGratitudeRandomByTypeQuery(GratitudeType type, string language = "en", string name = "Alice", string signature = "Bob")
         {
+            Language = language.ToLower();
             Type = type;
             Name = name;
             Signature = signature;
         }
 
+        public string Language { get; }
         public GratitudeType Type { get; set; }
         public string Name { get; }
         public string Signature { get; }
