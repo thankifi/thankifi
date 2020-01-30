960aea522f2cb52d62854e7d12b1ca7403dcb616
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-30 12:23:34 +0100

    Added GratitudeTypeConfiguration

---
+++ src/TaaS.Persistence/Configuration/GratitudeTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaaS.Core.Entity;
+
+namespace TaaS.Persistence.Configuration
+{
+    public class GratitudeTypeConfiguration : IEntityTypeConfiguration<Gratitude>
+    {
+        public void Configure(EntityTypeBuilder<Gratitude> builder)
+        {
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
