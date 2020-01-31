068305ef7fbf21e557c67bcb60eddd95e921ac3b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-31 10:32:46 +0100

    Updated entities

---
+++ src/TaaS.Core.Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TaaS.Core.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<GratitudeCategory> Gratitudes { get; set; }
+    }
+}
--- src/TaaS.Core.Entity/Gratitude.cs
+++ src/TaaS.Core.Entity/Gratitude.cs
@@ -1,11 +1,14 @@
-﻿namespace TaaS.Core.Entity
+﻿using System.Collections.Generic;
+
+namespace TaaS.Core.Entity
 {
     public class Gratitude
     {
         public int Id { get; set; }
         public string Language { get; set; }
-        public GratitudeType Type { get; set; }
         public string Text { get; set; }
+        public GratitudeType Type { get; set; }
+        public List<GratitudeCategory> Categories { get; set; }
     }
 
     public enum GratitudeType
