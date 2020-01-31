0b24d375db341fe35a5cb227e72656a245b8cef7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-31 12:31:37 +0100

    Implemented Client and Service

---
+++ src/TaaS.Infrastructure.Implementation/Client/ImportClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using TaaS.Infrastructure.Contract.Client;
+using TaaS.Infrastructure.Contract.Model;
+
+namespace TaaS.Infrastructure.Implementation.Client
+{
+    public class ImportClient : IImporterClient
+    {
+        protected readonly ILogger<ImportClient> Logger;
+        protected readonly HttpClient Client;
+        protected readonly string? Version;
+
+        public ImportClient(ILogger<ImportClient> logger, HttpClient client, IConfiguration configuration)
+        {
+            client.BaseAddress = new Uri(configuration["IMPORTER_SOURCE_URL"]);
+            Version = configuration["IMPORTER_DB_VERSION"];
+            Logger = logger;
+            Client = client;
+        }
+
+        public async Task<ImportResponse> GetData()
+        {
+            var requestResponse = await Client.GetAsync(Version != null ? $"/data_{Version}.json" : "/data.json");
+
+            requestResponse.EnsureSuccessStatusCode();
+            
+            var importData = await requestResponse.Content.ReadAsAsync<ImportResponse>();
+
+            return importData;
+        }
+    }
+}
---
+++ src/TaaS.Infrastructure.Implementation/Service/ImporterService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+using TaaS.Infrastructure.Contract.Client;
+using TaaS.Infrastructure.Contract.Model;
+using TaaS.Infrastructure.Contract.Service;
+using static OperationResult.Helpers;
+
+namespace TaaS.Infrastructure.Implementation.Service
+{
+    public class ImporterService : IImporterService
+    {
+        protected readonly ILogger<ImporterService> Logger;
+        protected readonly IImporterClient Client;
+
+        public ImporterService(ILogger<ImporterService> logger, IImporterClient client)
+        {
+            Logger = logger;
+            Client = client;
+        }
+
+        public async Task<Result<(List<Gratitude>, List<Category>), string>> Fetch()
+        {
+            try
+            {
+                var rawData = await Client.GetData();
+
+                return Ok((rawData.Gratitudes, rawData.Categories));
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e,"Error while fetching data from source.");
+                
+                return Error(e.Message);
+            }
+        }
+    }
+}
