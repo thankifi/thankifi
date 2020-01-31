5f17d13c32010b24d936a2c44cdaef6b3005ee40
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-31 13:36:10 +0100

    Implemented ImportGratitudesCommand handler

--- src/TaaS.Core.Domain/Gratitude/Command/ImportGratitudes/ImportGratitudesCommand.cs
+++ src/TaaS.Core.Domain/Gratitude/Command/ImportGratitudes/ImportGratitudesCommand.cs
@@ -3,7 +3,7 @@ using OperationResult;
 
 namespace TaaS.Core.Domain.Gratitude.Command.ImportGratitudes
 {
-    public class ImportGratitudesCommand : IRequest<Status<string>>
+    public class ImportGratitudesCommand : IRequest<Unit>
     {
         
     }
--- src/TaaS.Core.Domain/Gratitude/Command/ImportGratitudes/ImportGratitudesCommandHandler.cs
+++ src/TaaS.Core.Domain/Gratitude/Command/ImportGratitudes/ImportGratitudesCommandHandler.cs
@@ -1,15 +1,103 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using OperationResult;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TaaS.Core.Domain.Gratitude.Notification;
+using TaaS.Core.Entity;
+using TaaS.Infrastructure.Contract.Service;
+using TaaS.Persistence.Context;
 
 namespace TaaS.Core.Domain.Gratitude.Command.ImportGratitudes
 {
-    public class ImportGratitudesCommandHandler : IRequestHandler<ImportGratitudesCommand, Status<string>>
+    public class ImportGratitudesCommandHandler : IRequestHandler<ImportGratitudesCommand, Unit>
     {
-        public Task<Status<string>> Handle(ImportGratitudesCommand request, CancellationToken cancellationToken)
+        protected readonly ILogger<ImportGratitudesCommandHandler> Logger;
+        protected readonly IImporterService ImporterService;
+        protected readonly TaaSDbContext Context;
+        protected readonly IMediator Mediator;
+
+        public ImportGratitudesCommandHandler(ILogger<ImportGratitudesCommandHandler> logger, IImporterService importerService, TaaSDbContext context, IMediator mediator)
         {
-            throw new System.NotImplementedException();
+            Logger = logger;
+            ImporterService = importerService;
+            Context = context;
+            Mediator = mediator;
+        }
+
+        public async Task<Unit> Handle(ImportGratitudesCommand request, CancellationToken cancellationToken)
+        {
+            var startTime = DateTime.UtcNow;
+
+            var fetchResult = await ImporterService.Fetch();
+            
+            if (fetchResult.IsError)
+            {
+                await Mediator.Publish(new ImportErrorNotification(startTime, fetchResult.Error), cancellationToken);
+            }
+
+            var (gratitudes, categories) = fetchResult.Value;
+            
+            await using (var transaction = await Context.Database.BeginTransactionAsync(cancellationToken))
+            {
+                try
+                {
+                    var tableName = Context.Model.FindEntityType(typeof(GratitudeCategory)).GetTableName();
+                    await Context.Database.ExecuteSqlRawAsync($"DELETE FROM {tableName}", cancellationToken);
+                    
+                    tableName = Context.Model.FindEntityType(typeof(Category)).GetTableName();
+                    await Context.Database.ExecuteSqlRawAsync($"DELETE FROM {tableName}", cancellationToken);
+                    
+                    tableName = Context.Model.FindEntityType(typeof(Entity.Gratitude)).GetTableName();
+                    await Context.Database.ExecuteSqlRawAsync($"DELETE FROM {tableName}", cancellationToken);
+
+                    foreach (var category in categories)
+                    {
+                        await Context.Categories.AddAsync(new Category
+                        {
+                            Id = category.Id,
+                            Title = category.Title
+                        }, cancellationToken);
+                    }
+
+                    foreach (var gratitude in gratitudes)
+                    {
+                        var gratitudeEntity = new Entity.Gratitude
+                        {
+                            Id = gratitude.Id,
+                            Language = gratitude.Language,
+                            Type = Enum.Parse<GratitudeType>(gratitude.Type),
+                            Categories = new List<GratitudeCategory>()
+                        };
+
+                        foreach (var category in gratitude.Categories)
+                        {
+                            gratitudeEntity.Categories.Add(new GratitudeCategory{
+                                GratitudeId = gratitudeEntity.Id,
+                                CategoryId = categories.Single(c => string.Equals(c.Title, category, StringComparison.CurrentCultureIgnoreCase)).Id
+                            });
+                        }
+                        
+                        await Context.Gratitudes.AddAsync(gratitudeEntity, cancellationToken);
+                    }
+
+                    await Context.SaveChangesAsync(cancellationToken);
+                    
+                    await transaction.CommitAsync(cancellationToken);
+
+                    await Mediator.Publish(new ImportSuccessNotification(startTime), cancellationToken);
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError(e, "Unhandled error while updating database.");
+                    await Mediator.Publish(new ImportErrorNotification(startTime, e.Message), cancellationToken);
+                }
+            }
+            
+            return Unit.Value;
         }
     }
 }
