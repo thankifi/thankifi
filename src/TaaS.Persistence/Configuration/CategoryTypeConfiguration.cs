6dff17b042c1312e5a765a4d32bd0b9a7301824b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-31 11:09:35 +0100

    Added entity type configurations for new entities

---
+++ src/TaaS.Persistence/Configuration/CategoryTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaaS.Core.Entity;
+
+namespace TaaS.Persistence.Configuration
+{
+    public class CategoryTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
---
+++ src/TaaS.Persistence/Configuration/GratitudeCategoryTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaaS.Core.Entity;
+
+namespace TaaS.Persistence.Configuration
+{
+    public class GratitudeCategoryTypeConfiguration : IEntityTypeConfiguration<GratitudeCategory>
+    {
+        public void Configure(EntityTypeBuilder<GratitudeCategory> builder)
+        {
+            builder.HasOne(e => e.Category)
+                .WithMany(e => e.Gratitudes)
+                .HasForeignKey(e => e.CategoryId);
+
+            builder.HasOne(e => e.Gratitude)
+                .WithMany(e => e.Categories)
+                .HasForeignKey(e => e.GratitudeId);
+        }
+    }
+}
