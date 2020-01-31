9cc44ceeecd851a7aec9e2b0e416f5267eec8799
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-01-31 12:31:27 +0100

    Added infrastructure interfaces

---
+++ src/TaaS.Infrastructure.Contract/Client/IImporterClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TaaS.Infrastructure.Contract.Model;
+
+namespace TaaS.Infrastructure.Contract.Client
+{
+    public interface IImporterClient
+    {
+        Task<ImportResponse> GetData();
+    }
+}
---
+++ src/TaaS.Infrastructure.Contract/Service/IImporterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OperationResult;
+using TaaS.Infrastructure.Contract.Model;
+
+namespace TaaS.Infrastructure.Contract.Service
+{
+    public interface IImporterService
+    {
+        Task<Result<(List<Gratitude>, List<Category>), string>> Fetch();
+    }
+}
