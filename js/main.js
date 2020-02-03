f0dfe805a7ead96620b8f0819a3e9be1f4fa40d1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 22:35:09 +0100

    WIP

---
+++ js/main.js
@@ -0,0 +1,5 @@
+'use strict'
+
+console.log('There is nothing to see here lulz - Lucas\n\n')
+console.log('As you are here, why don\'t you give Archlinux a try? A simple, lightweight Linux distribution that tries to Keep It Simple!')
+console.log('https://www.archlinux.org/')
