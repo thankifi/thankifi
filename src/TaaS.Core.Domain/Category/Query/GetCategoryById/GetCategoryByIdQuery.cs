2f2a675016d1c38222ed62af533a75b7bd444da5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 15:01:44 +0100

    Added query for GetCategoryById and it's handler

---
+++ src/TaaS.Core.Domain/Category/Query/GetCategoryById/GetCategoryByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace TaaS.Core.Domain.Category.Query.GetCategoryById
+{
+    public class GetCategoryByIdQuery : IRequest<(int, Entity.Category?)>
+    {
+        public GetCategoryByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ src/TaaS.Core.Domain/Category/Query/GetCategoryById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TaaS.Persistence.Context;
+
+namespace TaaS.Core.Domain.Category.Query.GetCategoryById
+{
+    public class GetCategoryByIdQueryHandler : IRequestHandler<GetCategoryByIdQuery, (int, Entity.Category?)>
+    {
+        protected readonly ILogger<GetCategoryByIdQueryHandler> Logger;
+        protected readonly TaaSDbContext Context;
+
+        public GetCategoryByIdQueryHandler(ILogger<GetCategoryByIdQueryHandler> logger, TaaSDbContext context)
+        {
+            Logger = logger;
+            Context = context;
+        }
+
+        public async Task<(int, Entity.Category?)> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var category = await Context.Categories.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            if (category == null)
+            {
+                return (1, null);
+            }
+
+            var count = await Context.GratitudeCategories.AsNoTracking()
+                .Where(gc => gc.CategoryId == request.Id)
+                .CountAsync(cancellationToken);
+
+            return (count, category);
+        }
+    }
+}
