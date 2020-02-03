4ac894eb9ced09dc10d3f8e76441de7281fcce40
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 13:46:53 +0100

    Renamed GratitudeResposne to GratitudeViewModel, added parse

--- src/TaaS.Api.WebApi/Model/V1/GratitudeViewModel.cs
+++ src/TaaS.Api.WebApi/Model/V1/GratitudeViewModel.cs
@@ -1,14 +1,24 @@
-﻿namespace TaaS.Api.WebApi.Model.V1
-{
-    public class GratitudeResponse
-    {
-        public GratitudeResponse(int id, string text)
-        {
-            Id = id;
-            Text = text;
-        }
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TaaS.Core.Entity;
 
+namespace TaaS.Api.WebApi.Model.V1
+{
+    public class GratitudeViewModel
+    {
         public int Id { get; set; }
         public string Text { get; set; }
+        public IEnumerable<string> Categories { get; set; }
+
+        public static GratitudeViewModel Parse(Gratitude gratitude)
+        {
+            return new GratitudeViewModel
+            {
+                Id = gratitude.Id,
+                Text = gratitude.Text,
+                Categories = gratitude.Categories.Select(c => c.Category.Title)
+            };
+        }
     }
 }
