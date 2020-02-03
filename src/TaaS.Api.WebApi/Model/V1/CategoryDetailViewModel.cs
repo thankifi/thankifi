36b8a076d796e8a02fdff1a1012494da98ec7a85
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 15:00:22 +0100

    Created CategoryDetailViewModel

---
+++ src/TaaS.Api.WebApi/Model/V1/CategoryDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using TaaS.Core.Entity;
+
+namespace TaaS.Api.WebApi.Model.V1
+{
+    public class CategoryDetailViewModel : CategoryViewModel
+    {
+        public int Total { get; set; }
+
+        public static CategoryDetailViewModel Parse(Category category, int total)
+        {
+            return new CategoryDetailViewModel
+            {
+                Id = category.Id,
+                Title = category.Title,
+                Total = total
+            };;
+        }
+    }
+}
