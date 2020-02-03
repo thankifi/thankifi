abaa12e3e9f2a66da0c81e874d893a7f8f70f3dc
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 14:08:53 +0100

    Created CategoryViewModel

---
+++ src/TaaS.Api.WebApi/Model/V1/CategoryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaaS.Core.Entity;
+
+namespace TaaS.Api.WebApi.Model.V1
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public static CategoryViewModel Parse(Category category)
+        {
+            return new CategoryViewModel
+            {
+                Id = category.Id,
+                Title = category.Title
+            };
+        }
+
+        public static IEnumerable<CategoryViewModel> Parse(IEnumerable<Category> categories)
+        {
+            return categories.Select(Parse);
+        }
+    }
+}
