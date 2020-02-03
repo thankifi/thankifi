1f29e5fe0a6e49c848f830f6585343bdedaa8bc6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 15:02:20 +0100

    Upadted CategoryController, added new method

--- src/TaaS.Api.WebApi/Controllers/V1/CategoryController.cs
+++ src/TaaS.Api.WebApi/Controllers/V1/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TaaS.Api.WebApi.Model.V1;
 using TaaS.Core.Domain.Category.Query.GetAllCategories;
+using TaaS.Core.Domain.Category.Query.GetCategoryById;
 
 namespace TaaS.Api.WebApi.Controllers.V1
 {
@@ -36,6 +38,29 @@ namespace TaaS.Api.WebApi.Controllers.V1
             var result = await Mediator.Send(new GetAllCategoriesQuery(), cancellationToken);
 
             return Ok(CategoryViewModel.Parse(result));
+        }
+
+        /// <summary>
+        /// Get a list of all the categories available in TaaS.
+        /// </summary>
+        /// <param name="id">Id of the category.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Detailed view of the category. Thanks!</response>
+        /// <response code="404">Category not found! Thanks! Thanks!</response>
+        [HttpGet, Route("{id}")]
+        [ProducesResponseType(typeof(CategoryDetailViewModel), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<IActionResult> GetById([FromRoute, Required] int id,
+            CancellationToken cancellationToken = default)
+        {
+            var (total, category) = await Mediator.Send(new GetCategoryByIdQuery(id), cancellationToken);
+
+            if (category != null)
+            {
+                return Ok(CategoryDetailViewModel.Parse(category, total));
+            }
+
+            return NotFound("Category Not Found.");
         }
     }
 }
