504e9b2d659f75be67735706bfbcc00f638e7fc1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-03 13:57:59 +0100

    Implemented handler for GetAllCategories query

---
+++ src/TaaS.Core.Domain/Category/Query/GetAllCategories/GetAllCategoriesQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TaaS.Persistence.Context;
+
+namespace TaaS.Core.Domain.Category.Query.GetAllCategories
+{
+    public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, IEnumerable<Entity.Category>>
+    {
+        protected readonly ILogger<GetAllCategoriesQueryHandler> Logger;
+        protected readonly TaaSDbContext Context;
+
+        public GetAllCategoriesQueryHandler(ILogger<GetAllCategoriesQueryHandler> logger, TaaSDbContext context)
+        {
+            Logger = logger;
+            Context = context;
+        }
+
+        public async Task<IEnumerable<Entity.Category>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            Logger.LogDebug("Requested categories list.");
+
+            var categories = await Context.Categories.AsNoTracking().ToListAsync(cancellationToken);
+
+            return categories;
+        }
+    }
+}
