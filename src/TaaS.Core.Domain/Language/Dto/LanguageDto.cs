a0b77c79674c38475a0e0b8eeb07a5870d0d70d3
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-04 11:37:02 +0100

    Added query for getting all languages available

---
+++ src/TaaS.Core.Domain/Language/Dto/LanguageDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TaaS.Core.Domain.Language.Dto
+{
+    public class LanguageDto
+    {
+        public LanguageDto(string code)
+        {
+            Code = code;
+            Reference = $"https://iso639-3.sil.org/code/{code}";
+        }
+
+        public string Code { get; }
+        public string Reference { get; }
+    }
+}
---
+++ src/TaaS.Core.Domain/Language/GetAllLanguages/GetAllLanguagesQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using TaaS.Core.Domain.Language.Dto;
+
+namespace TaaS.Core.Domain.Language.GetAllLanguages
+{
+    public class GetAllLanguagesQuery : IRequest<IEnumerable<LanguageDto>>
+    {
+        
+    }
+}
