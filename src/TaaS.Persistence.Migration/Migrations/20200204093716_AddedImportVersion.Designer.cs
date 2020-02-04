8a9596814d0f05f4084e23ee71f4ab6c4a634d76
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-04 10:38:58 +0100

    Updated migrations

--- src/TaaS.Persistence.Migration/Migrations/20200204093716_AddedImportVersion.Designer.cs
+++ src/TaaS.Persistence.Migration/Migrations/20200204093716_AddedImportVersion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaaS.Persistence.Context;
@@ -9,9 +10,10 @@ using TaaS.Persistence.Context;
 namespace TaaS.Persistence.Migration.Migrations
 {
     [DbContext(typeof(TaaSDbContext))]
-    partial class TaaSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200204093716_AddedImportVersion")]
+    partial class AddedImportVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TaaS.Persistence.Migration/Migrations/20200204093716_AddedImportVersion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaaS.Persistence.Migration.Migrations
+{
+    public partial class AddedImportVersion : Microsoft.EntityFrameworkCore.Migrations.Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Version",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Version = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Version", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Version");
+        }
+    }
+}
