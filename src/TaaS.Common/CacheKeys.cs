19b91f1cff761f5e1c481298df32c1797384e7a7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-04 15:34:55 +0100

    Added new cache keys

--- src/TaaS.Common/CacheKeys.cs
+++ src/TaaS.Common/CacheKeys.cs
@@ -3,6 +3,7 @@
     public class CacheKeys
     {
         public static string CategoryViewModelList => "_CategoryViewModelList";
+        public static string CategoryDetailViewModel(object extraIdentifier) => $"_CategoryDetailViewModel_{extraIdentifier}";
         public static string LanguageViewModelList => "_LanguageViewModelList";
     }
 }
