eeb52406560ff7503cdd1a2489ed7781b71f3a02
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-04 15:34:48 +0100

    Updated Parse methods

--- src/TaaS.Api.WebApi/Model/V1/CategoryDetailViewModel.cs
+++ src/TaaS.Api.WebApi/Model/V1/CategoryDetailViewModel.cs
@@ -1,19 +1,20 @@
-﻿using TaaS.Core.Entity;
+﻿using TaaS.Core.Domain.Category.Dto;
+using TaaS.Core.Entity;
 
 namespace TaaS.Api.WebApi.Model.V1
 {
     public class CategoryDetailViewModel : CategoryViewModel
     {
-        public int Total { get; set; }
+        public int TotalGratitudes { get; set; }
 
-        public static CategoryDetailViewModel Parse(Category category, int total)
+        public static CategoryDetailViewModel Parse(CategoryDetailDto categoryDetailDto)
         {
             return new CategoryDetailViewModel
             {
-                Id = category.Id,
-                Title = category.Title,
-                Total = total
-            };;
+                Id = categoryDetailDto.Id,
+                Title = categoryDetailDto.Title,
+                TotalGratitudes = categoryDetailDto.TotalGratitudes
+            };
         }
     }
 }
--- src/TaaS.Api.WebApi/Model/V1/CategoryViewModel.cs
+++ src/TaaS.Api.WebApi/Model/V1/CategoryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using TaaS.Core.Domain.Category.Dto;
 using TaaS.Core.Entity;
 
 namespace TaaS.Api.WebApi.Model.V1
@@ -9,7 +10,7 @@ namespace TaaS.Api.WebApi.Model.V1
         public int Id { get; set; }
         public string Title { get; set; }
 
-        public static CategoryViewModel Parse(Category category)
+        public static CategoryViewModel Parse(CategoryDto category)
         {
             return new CategoryViewModel
             {
@@ -18,7 +19,7 @@ namespace TaaS.Api.WebApi.Model.V1
             };
         }
 
-        public static IEnumerable<CategoryViewModel> Parse(IEnumerable<Category> categories)
+        public static IEnumerable<CategoryViewModel> Parse(IEnumerable<CategoryDto> categories)
         {
             return categories.Select(Parse);
         }
