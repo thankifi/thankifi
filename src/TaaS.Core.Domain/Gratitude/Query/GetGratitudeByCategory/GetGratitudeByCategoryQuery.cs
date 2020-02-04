5bdd65d61d31a404874d13078592014b938e5568
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-04 15:06:08 +0100

    Implemented query for gratitudes by category

---
+++ src/TaaS.Core.Domain/Gratitude/Query/GetGratitudeByCategory/GetGratitudeByCategoryQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using TaaS.Core.Domain.Gratitude.Dto;
+
+namespace TaaS.Core.Domain.Gratitude.Query.GetGratitudeByCategory
+{
+    public class GetGratitudeByCategoryQuery : IRequest<GratitudeDto?>
+    {
+        public GetGratitudeByCategoryQuery(int? categoryId, string language, string name = "Alice", string signature = "Bob")
+        {
+            CategoryId = categoryId;
+            Language = language;
+            Name = name;
+            Signature = signature;
+        }
+
+        public GetGratitudeByCategoryQuery(string categoryTitle, string language, string name = "Alice", string signature = "Bob")
+        {
+            CategoryTitle = categoryTitle;
+            Language = language;
+            Name = name;
+            Signature = signature;
+        }
+
+        public int? CategoryId { get; }
+        public string? CategoryTitle { get; }
+        public string Language { get; }
+        public string Name { get; }
+        public string Signature { get; }
+    }
+}
---
+++ src/TaaS.Core.Domain/Gratitude/Query/GetGratitudeByCategory/GetGratitudeByCategoryQueryHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TaaS.Common.Helper;
+using TaaS.Core.Domain.Gratitude.Dto;
+using TaaS.Core.Entity;
+using TaaS.Persistence.Context;
+
+namespace TaaS.Core.Domain.Gratitude.Query.GetGratitudeByCategory
+{
+    public class GetGratitudeByCategoryQueryHandler : IRequestHandler<GetGratitudeByCategoryQuery, GratitudeDto?>
+    {
+        protected readonly ILogger<GetGratitudeByCategoryQueryHandler> Logger;
+        protected readonly TaaSDbContext Context;
+
+        public GetGratitudeByCategoryQueryHandler(ILogger<GetGratitudeByCategoryQueryHandler> logger, TaaSDbContext context)
+        {
+            Logger = logger;
+            Context = context;
+        }
+
+        public async Task<GratitudeDto?> Handle(GetGratitudeByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            Logger.LogDebug("Requested random gratitude by category.");
+
+            var offset = RandomProvider.GetThreadRandom()?.Next(0, await Context.Gratitudes.AsNoTracking()
+                .Where(g => g.Language == request.Language)
+                .Where(g => g.Categories.Any(c => c.CategoryId == request.CategoryId || c.Category.Title == request.CategoryTitle))
+                .CountAsync(cancellationToken));
+
+            var gratitude = await Context.Gratitudes.AsNoTracking()
+                .Where(g => g.Language == request.Language)
+                .Where(g => g.Categories.Any(c => c.CategoryId == request.CategoryId || c.Category.Title == request.CategoryTitle))
+                .Skip(offset ?? 0)
+                .Select(g => new GratitudeDto
+                {
+                    Id = g.Id,
+                    Language = g.Language,
+                    Text = g.Text,
+                    Customization = (int) g.Type,
+                    Categories = g.Categories.Select(gc => gc.Category.Title)
+                }).FirstOrDefaultAsync(cancellationToken);
+
+
+            if (gratitude != null)
+            {
+                gratitude.Text = gratitude.Customization switch
+                {
+                    (int) GratitudeType.Basic => gratitude.Text,
+                    (int) GratitudeType.Named => gratitude.Text.Replace("{{NAME}}", request.Name),
+                    (int) GratitudeType.Signed => gratitude.Text.Replace("{{SIGNATURE}}", request.Signature),
+                    (int) GratitudeType.NamedAndSigned => gratitude.Text.Replace("{{NAME}}", request.Name)
+                        .Replace("{{SIGNATURE}}", request.Signature),
+                    _ => gratitude.Text
+                };
+            }
+
+            return gratitude;        }
+    }
+}
