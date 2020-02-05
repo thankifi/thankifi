466ee9f5ce541417796ba389c674703d6c9d6249
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-05 13:04:56 +0100

    Updated ThanksController to use new queries

--- src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
+++ src/TaaS.Api.WebApi/Controllers/V1/ThanksController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Threading;
@@ -7,6 +8,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TaaS.Api.WebApi.Model.V1;
+using TaaS.Core.Domain.Gratitude.Query.GetGratitude;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitudeByCategory;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitudeById;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitudeRandom;
@@ -31,112 +33,60 @@ namespace TaaS.Api.WebApi.Controllers.V1
         }
 
         /// <summary>
-        /// Get a gratitude sentence by Id, optionally specify a name and a signature. Thanks!
-        /// </summary>
-        /// <param name="id">Id of the gratitude sentence.</param>
-        /// <param name="name">Name of the person receiving the expression of gratitude.</param>
-        /// <param name="signature">Name of the person who expresses their gratitude.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Gratitude sentence. Thanks!</response>
-        /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("{id}")]
-        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetById(
-            [FromRoute, Required] int id,
-            [FromQuery, DefaultValue("Alice")] string name = "Alice",
-            [FromQuery, DefaultValue("Bob")] string signature = "Bob",
-            CancellationToken cancellationToken = default)
-        {
-            var result = await Mediator.Send(new GetGratitudeByIdQuery(id, name, signature), cancellationToken);
-
-            if (result != null)
-            {
-                return Ok(GratitudeViewModel.Parse(result));
-            }
-
-            return NotFound("Gratitude Not Found.");
-        }
-
-        /// <summary>
-        /// Get a random gratitude filtered by category id. Thanks!
-        /// </summary>
-        /// <param name="categoryId">Id of the category.</param>
-        /// <param name="name">Name of the person receiving the expression of gratitude.</param>
-        /// <param name="signature">Name of the person who expresses their gratitude.</param>
-        /// <param name="language">Language of the gratitude.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Gratitude sentence. Thanks!</response>
-        /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("{categoryId}/random")]
-        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandomByCategoryId(
-            [FromRoute, Required] int categoryId,
-            [FromQuery, DefaultValue("Alice")] string name = "Alice",
-            [FromQuery, DefaultValue("Bob")] string signature = "Bob",
-            [FromQuery, DefaultValue("eng")] string language = "eng",
-            CancellationToken cancellationToken = default)
-        {
-            var result = await Mediator.Send(new GetGratitudeByCategoryQuery(categoryId, language, name, signature), cancellationToken);
-
-            if (result != null)
-            {
-                return Ok(GratitudeViewModel.Parse(result));
-            }
-
-            return NotFound("Gratitude Not Found.");
-        }
-
-        /// <summary>
-        /// Get a random gratitude filtered by category title. Thanks!
-        /// </summary>
-        /// <param name="categoryTitle">Title of the category.</param>
-        /// <param name="name">Name of the person receiving the expression of gratitude.</param>
-        /// <param name="signature">Name of the person who expresses their gratitude.</param>
-        /// <param name="language">Language of the gratitude.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Gratitude sentence. Thanks!</response>
-        /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("{categoryTitle}/random")]
-        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandomByCategoryTitle(
-            [FromRoute, Required] string categoryTitle,
-            [FromQuery, DefaultValue("Alice")] string name = "Alice",
-            [FromQuery, DefaultValue("Bob")] string signature = "Bob",
-            [FromQuery, DefaultValue("eng")] string language = "eng",
-            CancellationToken cancellationToken = default)
-        {
-            var result = await Mediator.Send(new GetGratitudeByCategoryQuery(categoryTitle, language, name, signature), cancellationToken);
-
-            if (result != null)
-            {
-                return Ok(GratitudeViewModel.Parse(result));
-            }
-
-            return NotFound("Gratitude Not Found.");
-        }
-
-        /// <summary>
         /// Get a random gratitude sentence, optionally specify a name and a signature. Thanks!
         /// </summary>
         /// <param name="name">Name of the person receiving the expression of gratitude.</param>
         /// <param name="signature">Name of the person who expresses their gratitude.</param>
+        /// <param name="filters">Filter or filters to apply to the text. Filters available: shouting, mocking.</param>
         /// <param name="language">Language of the gratitude.</param>
         /// <param name="cancellationToken"></param>
         /// <response code="200">Gratitude sentence. Thanks!</response>
         /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("random")]
+        [HttpGet]
         [ProducesResponseType(typeof(GratitudeViewModel), 200)]
         [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandom(
+        public async Task<IActionResult> Get(
+            [FromQuery] string? name,
+            [FromQuery] string? signature,
+            [FromQuery] string[]? filters,
+            [FromQuery, DefaultValue("eng")] string language = "eng",
+            CancellationToken cancellationToken = default)
+        {
+            var result = await Mediator.Send(new GetGratitudeQuery
+            {
+                Name = name,
+                Signature = signature,
+                Language = language,
+                Filters = new List<string>(filters)
+            }, cancellationToken);
+            
+            if (result != null)
+            {
+                return Ok(GratitudeViewModel.Parse(result));
+            }
+
+            return NotFound("Gratitude Not Found.");
+        }
+        
+        /// <summary>
+        /// Get a gratitude sentence by Id, optionally specify a name and a signature. Thanks!
+        /// </summary>
+        /// <param name="gratitudeId">Id of the gratitude sentence.</param>
+        /// <param name="name">Name of the person receiving the expression of gratitude.</param>
+        /// <param name="signature">Name of the person who expresses their gratitude.</param>
+        /// <param name="cancellationToken"></param>
+        /// <response code="200">Gratitude sentence. Thanks!</response>
+        /// <response code="404">Gratitude not found! Thanks!</response>
+        [HttpGet, Route("{gratitudeId:int}")]
+        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<IActionResult> GetById(
+            [FromRoute, Required] int gratitudeId,
             [FromQuery, DefaultValue("Alice")] string name = "Alice",
             [FromQuery, DefaultValue("Bob")] string signature = "Bob",
-            [FromQuery, DefaultValue("eng")] string language = "eng",
             CancellationToken cancellationToken = default)
         {
-            var result = await Mediator.Send(new GetGratitudeRandomQuery(language, name, signature), cancellationToken);
+            var result = await Mediator.Send(new GetGratitudeByIdQuery(gratitudeId, name, signature), cancellationToken);
 
             if (result != null)
             {
@@ -147,103 +97,34 @@ namespace TaaS.Api.WebApi.Controllers.V1
         }
 
         /// <summary>
-        /// Get a random gratitude basic sentence. No names, no signature. Thanks!
+        /// Get a random gratitude filtered by category. Thanks!
         /// </summary>
-        /// <param name="language">Language of the gratitude.</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        /// <response code="200">Gratitude sentence. Thanks!</response>
-        /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("random/basic")]
-        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandomBasic(
-            [FromQuery, DefaultValue("eng")] string language = "eng",
-            CancellationToken cancellationToken = default)
-        {
-            var result = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.Basic, language), cancellationToken);
-
-            if (result != null)
-            {
-                return Ok(GratitudeViewModel.Parse(result));
-            }
-
-            return NotFound("Gratitude Not Found.");
-        }
-
-        /// <summary>
-        /// Get a random gratitude named sentence. Required name, no signature. Thanks!
-        /// </summary>
-        /// <param name="name">Name of the person receiving the expression of gratitude.</param>
-        /// <param name="language">Language of the gratitude.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Gratitude sentence. Thanks!</response>
-        /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("random/named")]
-        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandomNamed(
-            [FromQuery, Required] string name,
-            [FromQuery, DefaultValue("eng")] string language = "eng",
-            CancellationToken cancellationToken = default)
-        {
-            var result = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.Named, name), cancellationToken);
-
-            if (result != null)
-            {
-                return Ok(GratitudeViewModel.Parse(result));
-            }
-
-            return NotFound("Gratitude Not Found.");
-        }
-
-        /// <summary>
-        /// Get a random gratitude signed sentence. No name, required signature. Thanks!
-        /// </summary>
-        /// <param name="signature">Name of the person who expresses their gratitude.</param>
-        /// <param name="language">Language of the gratitude.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Gratitude sentence. Thanks!</response>
-        /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("random/signed")]
-        [ProducesResponseType(typeof(GratitudeViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandomSigned(
-            [FromQuery, Required] string signature,
-            [FromQuery, DefaultValue("eng")] string language = "eng",
-            CancellationToken cancellationToken = default)
-        {
-            var result = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.Signed, language, signature: signature),
-                cancellationToken);
-
-            if (result != null)
-            {
-                return Ok(GratitudeViewModel.Parse(result));
-            }
-
-            return NotFound("Gratitude Not Found.");
-        }
-
-        /// <summary>
-        /// Get a random gratitude named and signed. Required name, required signature. Thanks!
-        /// </summary>
+        /// <param name="categoryName">Title of the category.</param>
         /// <param name="name">Name of the person receiving the expression of gratitude.</param>
         /// <param name="signature">Name of the person who expresses their gratitude.</param>
+        /// <param name="filters">Filter or filters to apply to the text. Filters available: shouting, mocking.</param>
         /// <param name="language">Language of the gratitude.</param>
         /// <param name="cancellationToken"></param>
         /// <response code="200">Gratitude sentence. Thanks!</response>
         /// <response code="404">Gratitude not found! Thanks!</response>
-        [HttpGet, Route("random/namedAndSigned")]
+        [HttpGet, Route("{categoryName}")]
         [ProducesResponseType(typeof(GratitudeViewModel), 200)]
         [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetRandomNamedAndSigned(
-            [FromQuery, Required] string name,
-            [FromQuery, Required] string signature,
+        public async Task<IActionResult> GetByCategoryTitle(
+            [FromRoute, Required] string categoryName,
+            [FromQuery] string? name,
+            [FromQuery] string? signature,
+            [FromQuery] string[]? filters,
             [FromQuery, DefaultValue("eng")] string language = "eng",
             CancellationToken cancellationToken = default)
         {
-            var result = await Mediator.Send(new GetGratitudeRandomByTypeQuery(GratitudeType.NamedAndSigned, language, name, signature),
-                cancellationToken);
+            var result = await Mediator.Send(new GetGratitudeByCategoryQuery
+            {
+                Name = name,
+                Signature = signature,
+                Language = language,
+                Filters = new List<string>(filters)
+            }, cancellationToken);
 
             if (result != null)
             {
