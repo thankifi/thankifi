0b9ab960eef611460712a45f32356306b6085ac1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-05 13:01:55 +0100

    Erased Type of Gratitude Entity

--- src/TaaS.Core.Entity/Gratitude.cs
+++ src/TaaS.Core.Entity/Gratitude.cs
@@ -7,15 +7,6 @@ namespace TaaS.Core.Entity
         public int Id { get; set; }
         public string Language { get; set; }
         public string Text { get; set; }
-        public GratitudeType Type { get; set; }
         public List<GratitudeCategory> Categories { get; set; }
-    }
-
-    public enum GratitudeType
-    {
-        Basic,
-        Named,
-        Signed,
-        NamedAndSigned
     }
 }
