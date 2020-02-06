3d74a2b49b4a395228dbb6153c2baf8ff93b0889
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-02-06 11:09:24 +0100

    Created LanguageDetailViewModel

---
+++ src/TaaS.Api.WebApi/Model/V1/LanguageDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using TaaS.Core.Domain.Language.Dto;
+
+namespace TaaS.Api.WebApi.Model.V1
+{
+    public class LanguageDetailViewModel : LanguageViewModel
+    {
+        public int TotalGratitudes { get; set; }
+        
+        public static LanguageDetailViewModel Parse(LanguageDetailDto languageDetailDto)
+        {
+            return new LanguageDetailViewModel
+            {
+                Code = languageDetailDto.Code,
+                Reference = languageDetailDto.Reference,
+                TotalGratitudes = languageDetailDto.TotalGratitudes
+            };
+        }
+    }
+}
