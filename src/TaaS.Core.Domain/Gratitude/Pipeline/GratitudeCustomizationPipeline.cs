a2c3a909bcfb3710a6df20b744d757e9d224cf09
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-07 15:32:31 +0100

    Implemented customization pipeline for all new queries

--- src/TaaS.Core.Domain/Gratitude/Pipeline/GratitudeCustomizationPipeline.cs
+++ src/TaaS.Core.Domain/Gratitude/Pipeline/GratitudeCustomizationPipeline.cs
@@ -1,14 +1,23 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using TaaS.Core.Domain.Gratitude.Dto;
+using TaaS.Core.Domain.Gratitude.Query.GetBulkAllFiltersGratitude;
+using TaaS.Core.Domain.Gratitude.Query.GetBulkAllFiltersGratitudeById;
+using TaaS.Core.Domain.Gratitude.Query.GetBulkGratitude;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitude;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitudeById;
 
 namespace TaaS.Core.Domain.Gratitude.Pipeline
 {
-    public class GratitudeCustomizationPipeline : IPipelineBehavior<GetGratitudeQuery, GratitudeDto?>,
-        IPipelineBehavior<GetGratitudeByIdQuery, GratitudeDto?>
+    public class GratitudeCustomizationPipeline :
+        IPipelineBehavior<GetGratitudeQuery, GratitudeDto?>,
+        IPipelineBehavior<GetGratitudeByIdQuery, GratitudeDto?>,
+        IPipelineBehavior<GetBulkGratitudeQuery, IEnumerable<GratitudeDto>>,
+        IPipelineBehavior<GetBulkAllFiltersGratitudeQuery, IEnumerable<GratitudeDto>>,
+        IPipelineBehavior<GetBulkAllFiltersGratitudeByIdQuery, IEnumerable<GratitudeDto>>
     {
         public async Task<GratitudeDto?> Handle(GetGratitudeQuery request, CancellationToken cancellationToken,
             RequestHandlerDelegate<GratitudeDto?> next)
@@ -40,6 +49,63 @@ namespace TaaS.Core.Domain.Gratitude.Pipeline
             return response;
         }
 
+        public async Task<IEnumerable<GratitudeDto>> Handle(GetBulkGratitudeQuery request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<IEnumerable<GratitudeDto>> next)
+        {
+            var response = await next();
+
+            var gratitudeDtos = response.ToList();
+
+            if (gratitudeDtos.Any())
+            {
+                gratitudeDtos.ForEach(dto =>
+                {
+                    dto.Text = ReplaceNameIfNecessary(dto.Text, request.Name);
+                    dto.Text = AddSignatureIfNecessary(dto.Text, request.Signature);
+                });
+            }
+
+            return gratitudeDtos;
+        }
+
+        public async Task<IEnumerable<GratitudeDto>> Handle(GetBulkAllFiltersGratitudeQuery request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<IEnumerable<GratitudeDto>> next)
+        {
+            var response = await next();
+
+            var gratitudeDtos = response.ToList();
+
+            if (gratitudeDtos.Any())
+            {
+                gratitudeDtos.ForEach(dto =>
+                {
+                    dto.Text = ReplaceNameIfNecessary(dto.Text, request.Name);
+                    dto.Text = AddSignatureIfNecessary(dto.Text, request.Signature);
+                });
+            }
+
+            return gratitudeDtos;
+        }
+
+        public async Task<IEnumerable<GratitudeDto>> Handle(GetBulkAllFiltersGratitudeByIdQuery request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<IEnumerable<GratitudeDto>> next)
+        {
+            var response = await next();
+
+            var gratitudeDtos = response.ToList();
+
+            if (gratitudeDtos.Any())
+            {
+                gratitudeDtos.ForEach(dto =>
+                {
+                    dto.Text = ReplaceNameIfNecessary(dto.Text, request.Name);
+                    dto.Text = AddSignatureIfNecessary(dto.Text, request.Signature);
+                });
+            }
+
+            return gratitudeDtos;
+        }
+
         private static string ReplaceNameIfNecessary(string text, string? name)
         {
             var firstBracket = text.IndexOf('{');
@@ -48,10 +114,10 @@ namespace TaaS.Core.Domain.Gratitude.Pipeline
             {
                 return text;
             }
-            
-            var lastBracket =  text.LastIndexOf('}');
 
-                var t = text.Substring(firstBracket, lastBracket - firstBracket + 1) switch
+            var lastBracket = text.LastIndexOf('}');
+
+            var t = text.Substring(firstBracket, lastBracket - firstBracket + 1) switch
             {
                 "{ {NAME} }" => text.Replace("{ {NAME} }", name != null ? $" {name} " : " "),
                 "{{NAME}}" => text.Replace("{{NAME}}", name != null ? $"{name}" : ""),
