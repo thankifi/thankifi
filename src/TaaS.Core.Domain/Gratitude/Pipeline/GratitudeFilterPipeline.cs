a4ab338217ac6d38007b31cbcbf56e2d57d55221
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-07 15:17:59 +0100

    Added filter for bulk get

--- src/TaaS.Core.Domain/Gratitude/Pipeline/GratitudeFilterPipeline.cs
+++ src/TaaS.Core.Domain/Gratitude/Pipeline/GratitudeFilterPipeline.cs
@@ -7,13 +7,16 @@ using System.Threading.Tasks;
 using MediatR;
 using TaaS.Common.Filter;
 using TaaS.Core.Domain.Gratitude.Dto;
+using TaaS.Core.Domain.Gratitude.Query.GetBulkGratitude;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitude;
 using TaaS.Core.Domain.Gratitude.Query.GetGratitudeById;
 
 namespace TaaS.Core.Domain.Gratitude.Pipeline
 {
-    public class GratitudeFilterPipeline : IPipelineBehavior<GetGratitudeQuery, GratitudeDto?>,
-        IPipelineBehavior<GetGratitudeByIdQuery, GratitudeDto?>
+    public class GratitudeFilterPipeline :
+        IPipelineBehavior<GetGratitudeQuery, GratitudeDto?>,
+        IPipelineBehavior<GetGratitudeByIdQuery, GratitudeDto?>,
+        IPipelineBehavior<GetBulkGratitudeQuery, IEnumerable<GratitudeDto>>
     {
         public async Task<GratitudeDto?> Handle(GetGratitudeQuery request, CancellationToken cancellationToken,
             RequestHandlerDelegate<GratitudeDto?> next)
@@ -39,6 +42,21 @@ namespace TaaS.Core.Domain.Gratitude.Pipeline
             }
 
             return response;
+        }
+
+        public async Task<IEnumerable<GratitudeDto>> Handle(GetBulkGratitudeQuery request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<IEnumerable<GratitudeDto>> next)
+        {
+            var response = await next();
+
+            var gratitudeDtos = response.ToList();
+            
+            if (gratitudeDtos.Any())
+            {
+                gratitudeDtos.ForEach(dto => dto.Text = ApplyFilters(dto.Text, request.Filters));
+            }
+
+            return gratitudeDtos;
         }
 
         private static string ApplyFilters(string text, IEnumerable<string> requestFilters)
