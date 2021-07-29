1b55ff7cf0a422bd0a97d93382b090efa9709dc7
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-07-29 18:15:06 +0300

    feat: improve logging and startup

--- src/Thankifi.Api/Program.cs
+++ src/Thankifi.Api/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection;
+using Amazon;
+using Amazon.CloudWatchLogs;
+using Amazon.Runtime;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions.EntityFrameworkCore.Destructurers;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.AwsCloudWatch;
+using Serilog.Sinks.AwsCloudWatch.LogStreamNameProvider;
 using Thankifi.Api.Configuration;
+using Thankifi.Common.Logging;
 
 namespace Thankifi.Api
 {
@@ -11,34 +26,47 @@ namespace Thankifi.Api
     {
         public static void Main(string[] args)
         {
-            Log.Logger = ConfigurationExtension.LoadLogger(Configuration);
-
+            var assembly = Assembly.GetCallingAssembly().GetName().Name;
+            
             try
             {
                 var host = CreateHostBuilder(args).Build();
-
-                Log.Information("Starting TaaS");
+                
+                if (Log.Logger is not null)
+                {
+                    Log.Information("Starting {Service}", assembly);
+                }
 
                 host.Run();
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "TaaS terminated unexpectedly");
+                if (Log.Logger is not null)
+                {
+                    Log.Fatal(ex, "{Service} terminated unexpectedly", assembly);
+                }
             }
             finally
             {
-                Log.CloseAndFlush();
+                
+                if (Log.Logger is not null)
+                {
+                    Log.CloseAndFlush();
+                }
             }
         }
-
-        private static IConfiguration Configuration { get; } = ConfigurationExtension.LoadConfiguration();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseSerilog();
+                    webBuilder.AddCommonLogging();
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("appsettings.Local.json", true);
+                    builder.AddEnvironmentVariables();
                 });
     }
 }
