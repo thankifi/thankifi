80ba7e91f6f39578ed68308c64a1bc7a7e68d7fa
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-07-30 19:22:24 +0300

    feat: refactor category api definition

--- src/Thankifi.Api/Controllers/V1/CategoryController.cs
+++ src/Thankifi.Api/Controllers/V1/CategoryController.cs
@@ -4,9 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Thankifi.Api.Model.V1;
+using Thankifi.Api.Model.V1.Responses;
 using Thankifi.Core.Domain.Category.Query.GetAllCategories;
 using Thankifi.Core.Domain.Category.Query.GetCategoryById;
 using Thankifi.Common;
@@ -27,69 +29,27 @@ namespace Thankifi.Api.Controllers.V1
             _cache = memoryCache;
             _mediator = mediator;
         }
-
+        
         /// <summary>
-        /// Get a list of all the categories available. Thanks!
+        /// Retrieve a paginated list of all the available categories. Thanks!
         /// </summary>
-        /// <param name="language">Filter by language.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Categories list. Thanks!</response>
-        [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<CategoryViewModel>), 200)]
-        public async Task<IActionResult> Get(
-            [FromQuery] string? language,
-            CancellationToken cancellationToken)
+        [HttpGet(nameof(RetrieveAllCategories))]
+        [ProducesResponseType(typeof(IEnumerable<CategoryViewModel>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> RetrieveAllCategories(CancellationToken cancellationToken)
         {
-            if (!_cache.TryGetValue(CacheKeys.CategoryViewModelList(language), out IEnumerable<CategoryViewModel> cacheEntry))
-            {
-                var result = await _mediator.Send(new GetAllCategoriesQuery
-                {
-                    Language = language
-                }, cancellationToken);
-                
-                cacheEntry = CategoryViewModel.Parse(result);
-
-                _cache.Set(CacheKeys.CategoryViewModelList(language), cacheEntry, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromHours(12)));
-            }
-            
-            return Ok(cacheEntry);
+            throw new NotImplementedException();
         }
 
         /// <summary>
-        /// Get detailed category. Includes number of items. Thanks!
+        /// Retrieve a detail view of a category and a paginated list of gratitudes for the specified category.
+        /// Optionally specify a subject, a signature, flavours and languages. Thanks!
         /// </summary>
-        /// <param name="categoryId">Id of the category.</param>
-        /// <param name="language">Filter by language.</param>
-        /// <param name="cancellationToken"></param>
-        /// <response code="200">Detailed view of the category. Thanks!</response>
-        /// <response code="404">Category not found! Thanks!</response>
-        [HttpGet, Route("{categoryId}")]
-        [ProducesResponseType(typeof(CategoryDetailViewModel), 200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> GetById(
-            [FromRoute, Required] int categoryId,
-            [FromQuery] string? language,
-            CancellationToken cancellationToken)
+        [HttpGet("{category:required}", Name = nameof(RetrieveByCategorySlug))]
+        [ProducesResponseType(typeof(CategoryDetailViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> RetrieveByCategorySlug([FromRoute, Required] string category, CancellationToken cancellationToken)
         {
-            if (!_cache.TryGetValue(CacheKeys.CategoryDetailViewModel(categoryId, language), out CategoryDetailViewModel cacheEntry))
-            {
-                var result = await _mediator.Send(new GetCategoryByIdQuery
-                {
-                    Id = categoryId,
-                    Language = language
-                }, cancellationToken);
-
-                if (result == null)
-                {
-                    return NotFound("Category Not Found.");
-                }
-                
-                cacheEntry = CategoryDetailViewModel.Parse(result);
-
-                _cache.Set(CacheKeys.CategoryDetailViewModel(categoryId, language), cacheEntry, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromHours(12)));
-            }
-            
-            return Ok(cacheEntry);
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/Thankifi.Api/Model/V1/Requests/Category/RetrieveByCategoryCodeQueryParameters.cs
@@ -0,0 +1,27 @@
+using Thankifi.Common.Pagination;
+
+namespace Thankifi.Api.Model.V1.Requests.Category
+{
+    public record RetrieveByCategoryCodeQueryParameters : QueryStringParameters
+    {
+        /// <summary>
+        /// Subject receiving the gratitude.
+        /// </summary>
+        public string? Subject { get; init; }
+
+        /// <summary>
+        /// Signature to attach at the end of the gratitude.
+        /// </summary>
+        public string? Signature { get; init; }
+
+        /// <summary>
+        /// List of flavours to apply to the gratitude. Currently available: shouting, mocking, leet.
+        /// </summary>
+        public string[]? Flavours { get; init; }
+
+        /// <summary>
+        /// Filter gratitudes by languages.
+        /// </summary>
+        public string[]? Languages { get; init; }
+    }
+}
