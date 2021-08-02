151ade313eb82fb51fdab3684bfc82843f16ee1d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-02 18:27:00 +0300

    feat: add new filter dependency

--- src/Thankifi.Api/Startup.cs
+++ src/Thankifi.Api/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Serilog;
 using Thankifi.Api.Configuration.Swagger;
+using Thankifi.Common.Filters;
 using Thankifi.Core.Domain.Gratitude.Dto;
 using Thankifi.Core.Domain.Gratitude.Pipeline;
 using Thankifi.Core.Domain.Gratitude.Query.GetBulkAllFiltersGratitude;
@@ -39,6 +40,8 @@ namespace Thankifi.Api
             services.AddHttpContextAccessor();
             services.AddControllers();
             services.AddMemoryCache();
+
+            services.AddFilters();
             
             #region Pipeline
 
