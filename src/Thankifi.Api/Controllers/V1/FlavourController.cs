7e75f2fb4811e92c0c46a9072205d626491d316b
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-03 17:54:37 +0300

    feat: implement flavour controller to list all available flavours

---
+++ src/Thankifi.Api/Controllers/V1/FlavourController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Thankifi.Api.Model.V1.Responses;
+using Thankifi.Common.Filters.Abstractions;
+
+namespace Thankifi.Api.Controllers.V1
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v{v:apiVersion}/[controller]")]
+    public class FlavourController : ControllerBase
+    {
+        private readonly IEnumerable<IFilter> _filters;
+
+        public FlavourController(IEnumerable<IFilter> filters)
+        {
+            _filters = filters;
+        }
+
+        /// <summary>
+        /// Retrieve a list of all the available flavours. Thanks!
+        /// </summary>
+        [HttpGet(Name = nameof(RetrieveAllCategories))]
+        [ProducesResponseType(typeof(IEnumerable<FlavourViewModel>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> RetrieveAllCategories(CancellationToken cancellationToken)
+        {
+            
+            return Ok(_filters.Select(f => new FlavourViewModel
+            {
+                Flavour = f.Identifier,
+                Text = string.Empty
+            }));
+        }
+
+    }
+}
