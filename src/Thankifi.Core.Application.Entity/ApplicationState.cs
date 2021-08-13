d813da9eb49bc7b55acd3ea73a0e0a7078cb63df
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-13 17:01:16 +0300

    feat: design application state entity

---
+++ src/Thankifi.Core.Application.Entity/ApplicationState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Thankifi.Core.Application.Entity
+{
+    public class ApplicationState
+    {
+        public int? DatasetVersion { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
